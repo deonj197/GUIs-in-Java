c2c0090b939cf0be188150397cdff8bee9694118
Deon Jackson Jr <deonj197@gmail.com>
2017-05-18 13:25:06 -0400

    Add files via upload

--- Button.java
+++ Button.java
@@ -4,22 +4,31 @@ import javax.swing.*;
 
 public class Button extends JPanel implements ActionListener {
 	
-	public void makeButton() {
+	protected JButton button1;
+	public Button() {
 /* Button management */
 		
-		JButton button1 = new JButton("Submit");
+		button1 = new JButton("Submit");
 		button1.setVerticalTextPosition(AbstractButton.CENTER);
-		button1.setHorizontalTextPosition(SwingConstants.CENTER);
+		button1.setHorizontalTextPosition(AbstractButton.CENTER);
 		button1.setMnemonic(KeyEvent.VK_M);
 		button1.setActionCommand("submit");
-		
+		button1.setBackground(new Color(255,209,164));
 	    button1.addActionListener(this);
 		add(button1);
 	}
-	@Override
+	
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-
+		if ("submit".equals(arg0.getActionCommand())) {
+            
+            button1.setEnabled(false);
+            
+        } else {
+            
+            button1.setEnabled(true);
+        }
+           
 	}
 
 }
